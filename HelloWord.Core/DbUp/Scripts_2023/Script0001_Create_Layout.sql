e9078ebacc7f3df4166a944847847fb0dfae220d
Simon <simoonf@vip.qq.com>
2023-11-30 10:47:38 +0800

    feat: posts 表时间字段类型调整

--- HelloWord.Core/DbUp/Scripts_2023/Script0001_Create_Layout.sql
+++ HelloWord.Core/DbUp/Scripts_2023/Script0001_Create_Layout.sql
@@ -2,7 +2,7 @@ create table if not exists posts (
     id int auto_increment primary key,
     title text not null,
     content mediumtext null ,
-    create_at int not null,
-    update_at int null
+    create_at datetime not null,
+    update_at datetime null
 ) engine = InnoDB
 default charset = utf8mb4;
